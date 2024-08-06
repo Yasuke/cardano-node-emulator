97d6fa1b0c76963ab4df55a5aa2d51458f118fc5
Sky O <soraskymizu@gmail.com>
2024-08-06 22:59:59 +0000

    Circumvent with check!

--- plutus-script-utils/src/Plutus/Script/Utils/V1/Generators.hs
+++ plutus-script-utils/src/Plutus/Script/Utils/V1/Generators.hs
@@ -22,7 +22,7 @@ import Plutus.Script.Utils.V1.Scripts qualified as Scripts
 
 alwaysSucceedValidator :: Ledger.Validator
 alwaysSucceedValidator =
-  Ledger.mkValidatorScript $$(PlutusTx.compile [||\_ _ _ -> unitval||])
+  Ledger.mkValidatorScript $$(PlutusTx.compile [||\_ _ _ -> PlutusTx.check True||])
 
 alwaysSucceedValidatorVersioned :: Ledger.Versioned Ledger.Validator
 alwaysSucceedValidatorVersioned = Ledger.Versioned alwaysSucceedValidator Ledger.PlutusV1
@@ -32,7 +32,7 @@ alwaysSucceedValidatorHash = Scripts.validatorHash alwaysSucceedValidator
 
 alwaysSucceedPolicy :: Ledger.MintingPolicy
 alwaysSucceedPolicy =
-  Ledger.mkMintingPolicyScript $$(PlutusTx.compile [||\_ _ -> unitval||])
+  Ledger.mkMintingPolicyScript $$(PlutusTx.compile [||\_ _ -> PlutusTx.check True||])
 
 alwaysSucceedPolicyVersioned :: Ledger.Versioned Ledger.MintingPolicy
 alwaysSucceedPolicyVersioned = Ledger.Versioned alwaysSucceedPolicy Ledger.PlutusV1
