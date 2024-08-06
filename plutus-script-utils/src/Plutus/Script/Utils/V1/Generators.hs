0efc528c5808eaa0e7913e70bd63f08b8191f6c6
Sky O <soraskymizu@gmail.com>
2024-08-06 22:38:32 +0000

    Use unit for V1 generators

--- plutus-script-utils/src/Plutus/Script/Utils/V1/Generators.hs
+++ plutus-script-utils/src/Plutus/Script/Utils/V1/Generators.hs
@@ -22,7 +22,7 @@ import Plutus.Script.Utils.V1.Scripts qualified as Scripts
 
 alwaysSucceedValidator :: Ledger.Validator
 alwaysSucceedValidator =
-  Ledger.mkValidatorScript $$(PlutusTx.compile [||\_ _ _ -> BuiltinUnit||])
+  Ledger.mkValidatorScript $$(PlutusTx.compile [||\_ _ _ -> ()||])
 
 alwaysSucceedValidatorVersioned :: Ledger.Versioned Ledger.Validator
 alwaysSucceedValidatorVersioned = Ledger.Versioned alwaysSucceedValidator Ledger.PlutusV1
@@ -32,7 +32,7 @@ alwaysSucceedValidatorHash = Scripts.validatorHash alwaysSucceedValidator
 
 alwaysSucceedPolicy :: Ledger.MintingPolicy
 alwaysSucceedPolicy =
-  Ledger.mkMintingPolicyScript $$(PlutusTx.compile [||\_ _ -> BuiltinUnit||])
+  Ledger.mkMintingPolicyScript $$(PlutusTx.compile [||\_ _ -> ()||])
 
 alwaysSucceedPolicyVersioned :: Ledger.Versioned Ledger.MintingPolicy
 alwaysSucceedPolicyVersioned = Ledger.Versioned alwaysSucceedPolicy Ledger.PlutusV1
